d05c1e6b37d4fb1fdbcd8237270d142dc7f6e4ce
JuanqQuijano <118690618+JuanqQuijano@users.noreply.github.com>
2025-07-09 17:58:00 -0500

    Update main.jsx

--- vibrared-frontend/src/main.jsx
+++ vibrared-frontend/src/main.jsx
@@ -2,13 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext'; // <-- Importar
 import App from './App.jsx';
-import './index.css'; // Importaremos los estilos aquí
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider> {/* <-- Envolver App */}
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
